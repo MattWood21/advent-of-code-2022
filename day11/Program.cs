b45bf589dcb690b5bd891e70b1138bf9b71f5f65
Matt Wood <matthewjameswood21@gmail.com>
2022-12-11 10:12:22 -0800

    Wild solution for part 2. Lol math.

--- day11/Program.cs
+++ day11/Program.cs
@@ -1,12 +1,26 @@
 ﻿using day11;
 
-var monkeys = Monkey.MonkeyFactory_ExampleInput();
+var monkeys = Monkey.MonkeyFactory_MyInput();
 var monkeyBusiness = new MonkeyBusiness(monkeys);
+
+var testableValues = monkeys.Select(x => x.TestDivisibleBy);
+long finalValue = 1;
+foreach (var value in testableValues)
+{
+    finalValue = value * finalValue;
+}
+var superModulo = finalValue;
+
 var rounds = 10000;
 
-for(int i = 0; i < rounds; i++)
+for(int i = 1; i <= rounds; i++)
 {
-    monkeyBusiness.ExecuteRound();
+    monkeyBusiness.ExecuteRound(superModulo);
+    if(i == 1 || i == 20 || i % 1000 == 0)
+    {
+        Console.WriteLine($@"Printing stats after {i} rounds");
+        monkeyBusiness.PrintItemsInspected();
+    }
 }
 
 Console.WriteLine($@"Total monkey business: {monkeyBusiness.CurrentMonkeyBusiness()}");
