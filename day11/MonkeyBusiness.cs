22d4103ce1f84d8c26cb64555fa7dd8e8f2d2d3e
Matt Wood <matthewjameswood21@gmail.com>
2022-12-11 09:05:28 -0800

    day11 part 1 done but not yet part 2

---
+++ day11/MonkeyBusiness.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day11
+{
+    public class MonkeyBusiness
+    {
+        private List<Monkey> _monkeys { get; set; } = new List<Monkey>();
+
+        public MonkeyBusiness(IEnumerable<Monkey> monkeys)
+        {
+            _monkeys = monkeys.ToList();
+        }
+
+        public void ExecuteRound()
+        {
+            var counter = 0;
+            foreach (var monkey in _monkeys)
+            {
+                Console.WriteLine($@"Monkey {counter} turn is starting");
+                var result = monkey.Go();
+                Console.WriteLine($@"Inspected {result.Count()} items. Sum of inspected items: {monkey.ItemsInspected}");
+                foreach (var item in result)
+                {
+                    Console.WriteLine($@"Monkey is throwing item {item.worry} to Monkey {item.targetMonkey}");
+                    _monkeys[item.targetMonkey].AddItem(item.worry);
+                }
+                counter++;
+            }
+        }
+
+        public long CurrentMonkeyBusiness() 
+        {
+            var sortedMonkeys = _monkeys.OrderByDescending(x => x.ItemsInspected).ToList();
+            return sortedMonkeys[0].ItemsInspected * sortedMonkeys[1].ItemsInspected;
+        }
+    }
+}
---
+++ day11/Program.cs
@@ -0,0 +1,12 @@
+﻿using day11;
+
+var monkeys = Monkey.MonkeyFactory_ExampleInput();
+var monkeyBusiness = new MonkeyBusiness(monkeys);
+var rounds = 10000;
+
+for(int i = 0; i < rounds; i++)
+{
+    monkeyBusiness.ExecuteRound();
+}
+
+Console.WriteLine($@"Total monkey business: {monkeyBusiness.CurrentMonkeyBusiness()}");
